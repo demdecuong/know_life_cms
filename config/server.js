20f2d2e4ff975ccc50b21989478a8ccd356c34db
Pham Nam <v.nampham@vinbrain.net>
2022-01-07 17:55:55 +0700

    enable auto reload

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  autoReload: { 'enabled': true } ,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ef722e91375e2ceb175e63eef13eca04'),
